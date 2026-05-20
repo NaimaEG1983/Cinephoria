a5f169eb7c73d9c4c5b7b42b2691dfdde52fda7e
NaimaBarthel <naima.elguelai@free.fr>
2026-05-20 17:32:17 +0200

    Ajout header et footer , fonction routes

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { AccueilComponent } from './pages/accueil/accueil.component';
 import { FilmsComponent } from './pages/films/films.component';
+import { ReservationComponent } from './pages/reservation/reservation.component';
 import { ConnectComponent } from './pages/connect/connect.component';
 import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
     { path: '', component: AccueilComponent },
     { path: 'films', component: FilmsComponent },
+    { path: 'reservation', component: ReservationComponent },
     { path: 'connexion', component: ConnectComponent },
     { path: 'contact', component: ContactComponent },
-    { path: '**', redirectTo: '' } //redirection vers l'accueilsi l'URL n'existe pas 
+    { path: '**', redirectTo: '' } //redirection vers l'accueil si l'URL n'existe pas 
 ];
